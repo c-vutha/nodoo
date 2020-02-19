f6561bba457555ed48e62d08e466d263a964bb44
Vutha Chhuon <vutha.c@phumdigital.com>
2020-02-19 15:32:24 +0700

    add read_group

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const Nodoo = require('./nodoo');
+const Nodoo = require("./nodoo");
 
-let url = 'http://localhost:8069';
-let db = 'odoo';
+let url = "http://localhost:8069";
+let db = "odoo12";
 const odoo = Nodoo(url, db);
 
 const playground = async () => {
-  let res = await odoo.login('admin', 'admin');
+  let res = await odoo.login("admin", "admin");
   //   console.log(res);
   res = await odoo.databases();
   //   console.log(res);
@@ -13,23 +13,34 @@ const playground = async () => {
   //   console.log(res);
   res = await odoo.serverInfo();
   //   console.log(res);
-  res = await odoo.searchRead('res.users', null, ['name', 'login', 'email']);
-  //   console.log(res);
-  res = await odoo.load('res.users', 2);
+  res = await odoo.searchRead("res.users", null, ["name", "login", "email"]);
+
+  res = await odoo.readGroup(
+    "hr.leave.report",
+    [["employee_id", "=", 1]],
+    ["employee_id", "number_of_days"],
+    ["employee_id", "holiday_status_id"],
+    null,
+    null,
+    null,
+    false
+  );
+  console.log(res);
+  res = await odoo.load("res.users", 2);
   //   console.log(res.login);
   const res_company = await odoo.search(
-    'res.partner',
-    [['is_company', '=', 'True']],
+    "res.partner",
+    [["is_company", "=", "True"]],
     {
       limit: 5
     }
   );
   console.log(res_company);
-  res = await odoo.searchCount('res.partner', [['is_company', '=', 'True']]);
+  res = await odoo.searchCount("res.partner", [["is_company", "=", "True"]]);
   console.log(res);
-  res = await odoo.read('res.partner', res_company, ['name']);
+  res = await odoo.read("res.partner", res_company, ["name"]);
   console.log(res);
-  res = await odoo.fieldsGet('res.bank', ['string', 'help', 'type']);
+  res = await odoo.fieldsGet("res.bank", ["string", "help", "type"]);
   console.log(res);
   res = await odoo.logout();
   //   console.log(res);
--- nodoo.js
+++ nodoo.js
@@ -1,8 +1,8 @@
-const http = require('axios').default;
-const uuid = require('uuid').v4;
+const http = require("axios").default;
+const uuid = require("uuid").v4;
 
-let url = 'http://localhost:8069';
-let db = 'odoo';
+let url = "http://localhost:8069";
+let db = "odoo12";
 let options;
 let session;
 
@@ -14,49 +14,67 @@ const searchRead = async (
   offset = 0,
   limit = null
 ) => {
-  const path = '/web/dataset/search_read';
+  const path = "/web/dataset/search_read";
   const params = { model };
-  if (domains) params['domain'] = domains;
-  if (fields) params['fields'] = fields;
-  if (offset) params['offset'] = offset;
-  if (limit) params['limit'] = limit;
-  if (sort) params['domain'] = sort;
+  if (domains) params["domain"] = domains;
+  if (fields) params["fields"] = fields;
+  if (offset) params["offset"] = offset;
+  if (limit) params["limit"] = limit;
+  if (sort) params["domain"] = sort;
   const response = await rpc(path, params);
   return response.data.result;
 };
 
 const load = async (model, id) => {
-  const path = '/web/dataset/load';
+  const path = "/web/dataset/load";
   const params = { model, id, fields: [] };
   const response = await rpc(path, params);
   return response.data.result.value;
 };
 
 const search = async (model, domains = [], options = {}) => {
-  return call_kw(model, 'search', [domains], options);
+  return call_kw(model, "search", [domains], options);
 };
 
 const searchCount = async (model, domains = []) => {
-  return call_kw(model, 'search_count', [domains], {});
+  return call_kw(model, "search_count", [domains], {});
 };
 
 const read = async (model, ids = [], fields = []) => {
-  return call_kw(model, 'read', [ids], { fields });
+  return call_kw(model, "read", [ids], { fields });
 };
 
-const fieldsGet = async (model, attributes = ['string', 'help', 'type']) => {
-  return call_kw(model, 'fields_get', [], { attributes });
+const readGroup = async (
+  model,
+  domain = null,
+  fields = null,
+  groupby = null,
+  offset = 0,
+  limit = null,
+  orderby = null,
+  lazy = true
+) => {
+  return call_kw(
+    model,
+    "read_group",
+    [domain, fields, groupby, offset, limit, orderby, lazy],
+    {}
+  );
+};
+
+const fieldsGet = async (model, attributes = ["string", "help", "type"]) => {
+  return call_kw(model, "fields_get", [], { attributes });
 };
 
 const call_kw = async (model, method, args = [], kwargs = {}) => {
-  const path = '/web/dataset/call_kw';
+  const path = "/web/dataset/call_kw";
   const params = { model, method: method, args: args, kwargs: kwargs };
   const response = await rpc(path, params);
   return response.data.result;
 };
 
 const login = async (login, password) => {
-  const path = '/web/session/authenticate';
+  const path = "/web/session/authenticate";
   const response = await rpc(path, { db, login, password });
   const cookie = parseCookie(response);
   this.session = cookie.session_id;
@@ -65,39 +83,39 @@ const login = async (login, password) => {
 };
 
 const logout = async () => {
-  const path = '/web/session/destroy';
+  const path = "/web/session/destroy";
   const response = await rpc(path, {});
   return response.data;
 };
 
 const databases = async () => {
-  const path = '/web/database/list';
+  const path = "/web/database/list";
   const response = await rpc(path, {});
   return response.data.result;
 };
 
 const serverInfo = async () => {
-  const path = '/web/webclient/version_info';
+  const path = "/web/webclient/version_info";
   const response = await rpc(path, {});
   return response.data.result;
 };
 
 const sessionInfo = async () => {
-  const path = '/web/session/get_session_info';
+  const path = "/web/session/get_session_info";
   const response = await rpc(path, {});
   return response.data.result;
 };
 
 const rpc = async (path, params) => {
   const headers = {
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json"
   };
   if (this.session) {
-    headers['X-Openerp-Session-Id'] = this.session;
+    headers["X-Openerp-Session-Id"] = this.session;
   }
   const body = {
-    jsonrpc: '2.0',
-    method: 'call',
+    jsonrpc: "2.0",
+    method: "call",
     params: params, //payload
     id: uuid()
   };
@@ -117,36 +135,36 @@ const sanitizeRpcError = response => {
   // Handle Odoo Error
   const error = data.error;
   var errorObj = {
-    title: '',
-    message: '',
+    title: "",
+    message: "",
     fullTrace: error
   };
   if (
     error.code === 200 &&
-    error.message === 'Odoo Server Error' &&
-    error.data.name === 'werkzeug.exceptions.NotFound'
+    error.message === "Odoo Server Error" &&
+    error.data.name === "werkzeug.exceptions.NotFound"
   ) {
-    errorObj.title = 'page_not_found';
-    errorObj.message = 'HTTP Error';
+    errorObj.title = "page_not_found";
+    errorObj.message = "HTTP Error";
   } else if (
-    (error.code === 100 && error.message === 'Odoo Session Expired') || //v8
+    (error.code === 100 && error.message === "Odoo Session Expired") || //v8
     (error.code === 300 &&
-      error.message === 'OpenERP WebClient Error' &&
-      error.data.debug.match('SessionExpiredException')) //v7
+      error.message === "OpenERP WebClient Error" &&
+      error.data.debug.match("SessionExpiredException")) //v7
   ) {
-    errorObj.title = 'session_expired';
+    errorObj.title = "session_expired";
     // cookies.delete_sessionId();
   } else if (
-    error.message === 'Odoo Server Error' &&
+    error.message === "Odoo Server Error" &&
     /FATAL:  database "(.+)" does not exist/.test(error.data.message)
   ) {
-    errorObj.title = 'database_not_found';
+    errorObj.title = "database_not_found";
     errorObj.message = error.data.message;
-  } else if (error.data.name === 'openerp.exceptions.AccessError') {
-    errorObj.title = 'AccessError';
+  } else if (error.data.name === "openerp.exceptions.AccessError") {
+    errorObj.title = "AccessError";
     errorObj.message = error.data.message;
   } else {
-    var split = ('' + error.data.fault_code).split('\n')[0].split(' -- ');
+    var split = ("" + error.data.fault_code).split("\n")[0].split(" -- ");
     if (split.length > 1) {
       error.type = split.shift();
       error.data.fault_code = error.data.fault_code.substr(
@@ -156,22 +174,22 @@ const sanitizeRpcError = response => {
 
     if (error.code === 200 && error.type) {
       errorObj.title = error.type;
-      errorObj.message = error.data.fault_code.replace(/\n/g, '<br />');
+      errorObj.message = error.data.fault_code.replace(/\n/g, "<br />");
     } else {
       errorObj.title = error.message;
-      errorObj.message = error.data.debug.replace(/\n/g, '<br />');
+      errorObj.message = error.data.debug.replace(/\n/g, "<br />");
     }
   }
 };
 
 const parseCookie = response => {
   // cookie string to object
-  const cookie = response.headers['set-cookie'][0]
-    .split(';')
+  const cookie = response.headers["set-cookie"][0]
+    .split(";")
     .reduce((res, c) => {
       const [key, val] = c
         .trim()
-        .split('=')
+        .split("=")
         .map(decodeURIComponent);
       try {
         return Object.assign(res, { [key]: JSON.parse(val) });
@@ -197,6 +215,7 @@ module.exports = (url, db, options = null) => {
     search,
     searchCount,
     read,
-    fieldsGet
+    fieldsGet,
+    readGroup
   };
 };
